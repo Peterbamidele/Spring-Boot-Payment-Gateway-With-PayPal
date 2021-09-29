77ebc2c8622c33bf5a50ffd051b398bbe7740bb5
Peterbamidele <peterbamidele@gmail.com>
2021-09-29 05:35:47 +0100

    setup

---
+++ src/main/java/com/paymentintegrationwithpaytminspringbootapplication/PaymentIntegrationWithPaytminSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.paymentintegrationwithpaytminspringbootapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentIntegrationWithPaytminSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentIntegrationWithPaytminSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paymentintegrationwithpaytminspringbootapplication/PaymentIntegrationWithPaytminSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paymentintegrationwithpaytminspringbootapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentIntegrationWithPaytminSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
